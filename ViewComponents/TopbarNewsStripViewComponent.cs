b05706d54e3959c2aba300d679c8c498390935a2
Alaa Hassan <alaa@example.com>
2025-08-01 22:38:35 +0300

    نسخة احتياطية قبل التعديلات – topbar issue

---
+++ ViewComponents/TopbarNewsStripViewComponent.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newfactjo.Data;
+using System.Linq;
+
+namespace Newfactjo.ViewComponents
+{
+    public class TopbarNewsStripViewComponent : ViewComponent
+    {
+        private readonly AppDbContext _context;
+
+        public TopbarNewsStripViewComponent(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var newsList = _context.NewsItems
+                .Where(n => n.CategoryId == 11 && n.IsPublished)
+                .OrderByDescending(n => n.PublishedDate)
+                .Take(5)
+                .ToList();
+
+            return View(newsList);
+        }
+    }
+}
